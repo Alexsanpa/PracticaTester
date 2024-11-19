110aee030cccede9e48c726dacb82c81a22f31cc
Alexander <alexsanpa028@gmail.com>
2024-11-18 21:46:19 -0500

    Ajustes de carpetas

--- src/test/java/starter/ui/registration/RegisterPage.java
+++
@@ -1,9 +0,0 @@
-package starter.ui.registration;
-
-
-import net.serenitybdd.core.pages.PageObject;
-import net.thucydides.core.annotations.DefaultUrl;
-
-@DefaultUrl("http://127.0.0.1:5500/registration/register.html")
-public class RegisterPage extends PageObject {
-}
